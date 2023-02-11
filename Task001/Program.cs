4878794e9e935951d57b6231340732f99e541c0f
Zarema <kzariema@mail.ru>
2023-02-11 17:42:10 +0300

    Corrected Task001

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -1,10 +1,19 @@
-﻿int number1 = 22;
-int number2 = 3;
-int number3 = 9;
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write ("Введите первое число ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write ("Введите второе число ");
+int number2 = int.Parse(Console.ReadLine());
+Console.Write ("Введите третье число ");
+int number3 = int.Parse(Console.ReadLine());
+
 int max = number1;
 
 if (number1>max) max = number1;
 if (number2>max) max = number2;
 if (number3>max) max = number3;
 
-    Console.WriteLine("max = " + max);
+Console.WriteLine("max = " + max);
