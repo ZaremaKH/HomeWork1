fbdf2ba2d42b695cfb39e25a0a76f3caa0568432
Zarema <kzariema@mail.ru>
2023-02-11 17:11:53 +0300

    Task001

---
+++ Task001/Program.cs
@@ -0,0 +1,10 @@
+﻿int number1 = 22;
+int number2 = 3;
+int number3 = 9;
+int max = number1;
+
+if (number1>max) max = number1;
+if (number2>max) max = number2;
+if (number3>max) max = number3;
+
+    Console.WriteLine("max = " + max);
